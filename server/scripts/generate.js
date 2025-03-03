1d54b1a57fe61b4d52634e451bd6fa92260532b8
Xolani-Qwabe <thabisoxolaniqwabe@gmail.com>
2025-03-03 17:12:46 +0200

    generate eth address method added next signature and recoverybit

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,19 +1,27 @@
-const {secp256k1 } = require("ethereum-cryptography/secp256k1");
-const { toHex } = require("ethereum-cryptography/utils");
+const { secp256k1 } = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { toHex, fromHex, utf8ToBytes, bytesToHex } = require("ethereum-cryptography/utils");
 
+// Generate a random private key
 const generatePrivateKey = () => {
-    return secp256k1.utils.randomPrivateKey();
+  return secp256k1.utils.randomPrivateKey();
 };
 
-const generatePublicKey = (privateKey)=>{
-    return secp256k1.getPublicKey(privateKey);
-}
+// Generate the corresponding public key from the private key
+const generatePublicKey = (privateKey) => {
+  return secp256k1.getPublicKey(privateKey);
+};
 
+// Generate the Ethereum address from the public key
+const getAddress = (publicKey) => {
+  const address = keccak256(publicKey.slice(1)); // remove the 0x04 prefix
+  return `0x${bytesToHex(address.slice(-20))}`; // Use the last 20 bytes to form the address
+};
 
-const privateKey = generatePrivateKey()
-const publicKey = generatePublicKey(privateKey)
+const privateKey = generatePrivateKey();
+const publicKey = generatePublicKey(privateKey);
+const address = getAddress(publicKey);
 
-console.log(`Private key : ${toHex(privateKey)} generated!`);
-console.log(`Public key: ${toHex(publicKey)} generated from private key: ${toHex(privateKey)} `)
-
-module.exports = { generatePrivateKey };
+console.log(`Private key: ${toHex(privateKey)} generated!`);
+console.log(`Public key: ${toHex(publicKey)} generated from private key: ${toHex(privateKey)} `);
+console.log(`Ethereum Address: ${address}`);
